30f47a6874d1a1a8c5a875c1127dbddf3c321ff1
1915387 <1915387@01-19636.pusd.dom>
2023-05-12 09:52:46 -0700

    Worked on Measure and measurement

--- BigO Starter/src/cse12pa4student/Main.java
+++ BigO Starter/src/cse12pa4student/Main.java
@@ -4,6 +4,8 @@ import static cse12pa4mysteries.Mysteries.*;
 public class Main {
 	
 	public static void main(String[] args) {
-		mysteryA(50);
+		Measure temp = new Measure();
+		//temp.measure(new String[]{"A", "B"}, 40, 100);
+		temp.measurementsToCSV(temp.measure(new String[]{"A", "B"}, 40, 100));
 	}
 }
--- BigO Starter/src/cse12pa4student/Measure.java
+++ BigO Starter/src/cse12pa4student/Measure.java
@@ -1,5 +1,6 @@
 package cse12pa4student;
 
+import java.util.ArrayList;
 import java.util.List;
 import static cse12pa4mysteries.Mysteries.*;
 import runtime.*;
@@ -9,16 +10,65 @@ public class Measure {
 
 	public static List<Measurement> measure(String[] toRun, int startN, int stopN) {
 		/** TODO **/
-		
+		int count = 0;
+		if(toRun == null) {
+			return null;
+		}
+		ArrayList<Measurement> result = new ArrayList<Measurement>();
 		//Example call to mystery method
-		mysteryA(50);
-		return null;
+		for(int i = 0; i < toRun.length; i++) {
+			for(int j = startN; j < stopN; j++) {
+				long startTime = System.nanoTime();
+				callMysteryMethod(toRun[i], j);
+				long endTime = System.nanoTime();
+				Measurement m = new Measurement(toRun[i], j, endTime - startTime);
+				result.add(m);
+				System.out.println("Execution Time: " + (endTime - startTime) + " nanoseconds" + " " + toRun[i]);
+				count++;
+			}
+		}
+		System.out.println(count);
+		return result;
 	}
+	
+    public static void callMysteryMethod(String letter, int n) {
+        switch (letter) {
+            case "A":
+                mysteryA(n);
+                break;
+            case "B":
+                mysteryB(n);
+                break;
+            case "C":
+                mysteryC(n);
+                break;
+            case "D":
+                mysteryD(n);
+                break;
+            case "E":
+                mysteryE(n);
+                break;
+            case "F":
+                mysteryF(n);
+                break;
+            default:
+                System.out.println("Invalid letter");
+                break;
+        }
+    }
 	
 
 	public static String measurementsToCSV(List<Measurement> toConvert) {
 		/** TODO **/
-		return null;
+		String result = "";
+		if(toConvert == null) {
+			return null;
+		} else
+		for(int i = 0; i < toConvert.size(); i++) {
+			result += toConvert.get(i).toString();
+			System.out.println();
+		}
+		return result;
 	}
 	
 		
